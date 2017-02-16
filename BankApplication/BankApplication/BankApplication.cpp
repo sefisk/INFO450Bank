e2a3e69fe3501b9b834567dbbbada9a5400118a6
sefisk <sefisk@gmail.com>
2017-02-15 23:08:05 -0500

    Update

--- BankApplication/BankApplication/BankApplication.cpp
+++ BankApplication/BankApplication/BankApplication.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
@@ -22,80 +23,84 @@ int main()
 
 		cout << "1: Deposit  \t" << "2: Withdraw  \t" << "3: Check  \t" << "4: Quit  \t" << endl;
 		cin >> acc_action;
-		cout << endl;
-		if (acc_action == 1)
-		{
 
-			before_balance = overal_total;
-
-			cout << "Deposit Amount: " << endl;
-			cin >> deposit_amount;
-
-
-			for (int i = d_num; i < MAX; i++)
-			{
-				d_s_a[i] = deposit_amount;
-			}
-
-			overal_total = deposit_amount + before_balance;
-			d_num = d_num + 1;
-			d = d + deposit_amount;
-		}
-		else if (acc_action == 2)
-		{
-			int withdraw_amount;
-			before_balance = overal_total;
-			cout << "Withdraw Amount: " << endl;
-			cin >> withdraw_amount;
-			for (int i = w_num; i < MAX; i++)
-			{
-				w_s_a[i] = withdraw_amount;
-			}
-			overal_total = before_balance - withdraw_amount;
-			w = w + withdraw_amount;
-			w_num = w_num + 1;
-		}
-		else if (acc_action == 3)
-		{
-			int check_amount;
-			before_balance = overal_total;
-			cout << "Check Amount: " << endl;
-			cin >> check_amount;
-			for (int i = c_num; i < MAX; i++)
-			{
-				c_s_a[i] = check_amount;
-			}
-			overal_total = before_balance - check_amount;
-			c = c + check_amount;
-			c_num = c_num + 1;
-		}
-		else if (acc_action == 4)
-		{
-
-			cout << "Bank Statement: " << endl << endl;
-			cout << "Beginning Balance: " << acc_balance << endl << endl;
-			cout << "Total Deposits: " << d << endl;
-			for (int i = 0; i < d_num; i++)
-			{
-				cout << d_s_a[i] << endl;
-			}
 			cout << endl;
-			cout << "Total Withdraws: " << w << endl;
-			for (int i = 0; i < w_num; i++)
+			if (acc_action == 1)
 			{
-				cout << w_s_a[i] << endl;
+
+				before_balance = overal_total;
+
+				cout << "Deposit Amount: ";
+				cin >> deposit_amount;
+
+
+				for (int i = d_num; i < MAX; i++)
+				{
+					d_s_a[i] = deposit_amount;
+				}
+
+				overal_total = deposit_amount + before_balance;
+				d_num = d_num + 1;
+				d = d + deposit_amount;
 			}
-			cout << endl;
-			cout << "Total Checks: " << c << endl;
-			for (int i = 0; i < c_num; i++)
+			else if (acc_action == 2)
 			{
-				cout << c_s_a[i] << endl;
+				int withdraw_amount;
+				before_balance = overal_total;
+				cout << "Withdraw Amount: ";
+				cin >> withdraw_amount;
+				for (int i = w_num; i < MAX; i++)
+				{
+					w_s_a[i] = withdraw_amount;
+				}
+				overal_total = before_balance - withdraw_amount;
+				w = w + withdraw_amount;
+				w_num = w_num + 1;
 			}
-			cout << endl;
-			cout << "----------------" << endl;
-			cout << "Ending Balance: " << overal_total + acc_balance << endl;
-			isReady = false;
-		}
+			else if (acc_action == 3)
+			{
+				int check_amount;
+				before_balance = overal_total;
+				cout << "Check Amount: ";
+				cin >> check_amount;
+				for (int i = c_num; i < MAX; i++)
+				{
+					c_s_a[i] = check_amount;
+				}
+				overal_total = before_balance - check_amount;
+				c = c + check_amount;
+				c_num = c_num + 1;
+			}
+			else if (acc_action == 4)
+			{
+				cout << "Bank Statement:" << endl << endl;
+				cout << "Beginning Balance: " << fixed << setprecision(2) << acc_balance << endl << endl;
+				cout << "Total Deposits: " << fixed << setprecision(2) << d << endl;
+				for (int i = 0; i < d_num; i++)
+				{
+					cout << "\t" << fixed << setprecision(2) << d_s_a[i] << endl;
+				}
+				cout << endl;
+				cout << "Total Withdraws: " << fixed << setprecision(2) << w << endl;
+				for (int i = 0; i < w_num; i++)
+				{
+					cout << "\t" << fixed << setprecision(2) << w_s_a[i] << endl;
+				}
+				cout << endl;
+				cout << "Total Checks: " << fixed << setprecision(2) << c << endl;
+				for (int i = 0; i < c_num; i++)
+				{
+					cout << "\t" << fixed << setprecision(2) << c_s_a[i] << endl;
+				}
+				cout << endl;
+				cout << "----------------" << endl;
+				cout << "Ending Balance: " << fixed << setprecision(2) << overal_total + acc_balance << endl;
+				isReady = false;
+			}
+			else {
+				cout << "Error: You can only use #1-4. Try again." << endl;
+			}
+		
 	}
 }
 
